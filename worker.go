043fba019896c46e8430a73fb7125352c4dc2717
Jay <wisehodl@pm.me>
2026-04-17 08:53:52 -0400

    Create worker module.

---
+++ worker.go
@@ -0,0 +1,19 @@
+package honeybee
+
+// Types
+
+// Worker Implementation
+
+type Worker interface{}
+
+// Base Struct
+
+type worker struct{}
+
+// Initiator Worker
+
+type InitiatorWorker struct{}
+
+// Responder Worker
+
+type ResponderWorker struct{}
